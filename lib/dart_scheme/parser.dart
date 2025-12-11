1a5b15fbd490a3d084219643a80f7bfca03521a2
Todd O'Bryan <toddobryan@gmail.com>
2025-12-11 14:12:12 -0500

    Still pivoting to R7RS

--- lib/dart_scheme/parser.dart
+++ lib/dart_scheme/parser.dart
@@ -103,11 +103,6 @@ class SchemeGrammar extends GrammarDefinition {
 
   // 7.1.4 Quasiquotations
 
-  Parser quasiquotation(int depth) => [
-    seq2(backtick(), ref1(qqTemplate, depth)),
-    seq4(lParen(), quasiquote(), ref1(qqTemplate, depth), rParen()),
-  ].toChoiceParser();
-
   Parser qqTemplate(int depth) {
     if (depth == 0) {
       return ref0(expression);
@@ -120,6 +115,11 @@ class SchemeGrammar extends GrammarDefinition {
       ].toChoiceParser();
     }
   }
+
+  Parser quasiquotation(int depth) => [
+    seq2(backtick(), ref1(qqTemplate, depth)),
+    seq4(lParen(), quasiquote(), ref1(qqTemplate, depth), rParen()),
+  ].toChoiceParser();
 
   Parser listQqTemplate(int depth) => [
     seq3(lParen(), ref1(qqTemplateOrSplice, depth).star(), rParen()),
@@ -155,14 +155,15 @@ class SchemeGrammar extends GrammarDefinition {
   // 7.1.3 Expressions
 
   Parser expression() => [
-    variable(),
-    ref0(literal),
+    identifier(),
+    literal(),
     ref0(procedureCall),
     ref0(lambdaExpression),
     ref0(conditional),
     ref0(assignment),
     ref0(macroUse),
     ref0(macroBlock),
+    ref0(includer),
   ].toChoiceParser();
 
   Parser literal() => [ref0(quotation), ref0(selfEvaluating)].toChoiceParser();
@@ -172,6 +173,7 @@ class SchemeGrammar extends GrammarDefinition {
     ref0(number),
     ref0(character),
     ref0(sString),
+    ref0(byteVector),
   ].toChoiceParser();
 
   Parser quotation() => [
@@ -190,9 +192,9 @@ class SchemeGrammar extends GrammarDefinition {
       seq5(lParen(), ref0(lambda), ref0(formals), ref0(body), ref0(rParen));
 
   Parser formals() => [
-    seq3(lParen(), variable().star(), ref0(rParen)),
-    variable(),
-    seq5(lParen(), variable().plus(), ref0(dot), variable(), ref0(rParen)),
+    seq3(lParen(), identifier().star(), ref0(rParen)),
+    identifier(),
+    seq5(lParen(), identifier().plus(), ref0(dot), identifier(), ref0(rParen)),
   ].toChoiceParser();
 
   Parser body() => seq2(ref0(definition).star(), ref0(sequence));
@@ -202,7 +204,7 @@ class SchemeGrammar extends GrammarDefinition {
   Parser command() => ref0(expression);
 
   Parser conditional() =>
-      seq5(lParen(), ref0(test), ref0(consequent), ref0(alternate), rParen());
+      seq6(lParen(), string("if"), ref0(test), ref0(consequent), ref0(alternate), rParen());
 
   Parser test() => ref0(expression);
 
@@ -211,11 +213,12 @@ class SchemeGrammar extends GrammarDefinition {
   Parser alternate() => ref0(expression).optional();
 
   Parser assignment() =>
-      seq5(lParen(), setBang(), variable(), ref0(expression), rParen());
+      seq5(lParen(), setBang(), identifier(), ref0(expression), rParen());
 
   // derived expressions come here
 
   Parser macroUse() => seq3(lParen(), keyword(), ref0(datum).star());
+  Parser keyword() => identifier();
 
   Parser macroBlock() => [
     seq7(
@@ -241,18 +244,32 @@ class SchemeGrammar extends GrammarDefinition {
   Parser syntaxSpec() =>
       seq4(lParen(), keyword(), ref0(transformerSpec), rParen());
 
-  Parser keyword() => identifier();
+  Parser includer() => [
+    seq4(lParen(), string("include"), sString().plus(), rParen()),
+    seq4(lParen(), string("include-ci"), sString().plus(), rParen()),
+  ].toChoiceParser();
 
   // 7.1.2 External Representations
 
-  Parser datum() => [ref0(simpleDatum), ref0(compoundDatum)].toChoiceParser();
+  Parser datum() => [
+    ref0(simpleDatum),
+    ref0(compoundDatum),
+    seq3(label(), char("="), ref0(datum)),
+    seq2(label(), char("#")),
+  ].toChoiceParser();
 
-  Parser simpleDatum() =>
-      [boolean(), number(), character(), sString(), symbol()].toChoiceParser();
+  Parser simpleDatum() => [
+    boolean(),
+    number(),
+    character(),
+    sString(),
+    symbol(),
+    byteVector(),
+  ].toChoiceParser();
 
   Parser symbol() => identifier();
 
-  Parser compoundDatum() => [ref0(list), ref0(vector)].toChoiceParser();
+  Parser compoundDatum() => [ref0(list), ref0(vector), ref0(abbreviation)].toChoiceParser();
 
   Parser list() => [
     seq3(lParen(), ref0(datum).star(), rParen()),
@@ -266,6 +283,7 @@ class SchemeGrammar extends GrammarDefinition {
       [quote(), backtick(), comma(), commaAt()].toChoiceParser();
 
   Parser vector() => seq3(hashParen(), ref0(datum).star(), rParen());
+  Parser label() => seq2(char("#"), uinteger(10));
 
   // 7.1.1 Lexical Structure
 
@@ -350,10 +368,10 @@ class SchemeGrammar extends GrammarDefinition {
   Parser initial() => [letter(), specialInitial()].toChoiceParser();
   Parser specialInitial() => anyOf("!\$%&*/:<=>?@^_~");
   Parser subsequent() =>
-      [initial(), digit(10), specialSubsequent()].toChoiceParser();
-  Parser digit() => pattern("0-9");
+      [initial(), digit10(), specialSubsequent()].toChoiceParser();
+  Parser digit10() => pattern("0-9");
   Parser hexDigit() =>
-      [digit(), pattern("a-f", ignoreCase: true)].toChoiceParser();
+      [digit10(), pattern("a-f", ignoreCase: true)].toChoiceParser();
   Parser explicitSign() => anyOf("+-");
   Parser specialSubsequent() => [explicitSign(), anyOf(".@")].toChoiceParser();
   Parser inlineHexEscape() => seq2(string("\\x"), hexScalarValue());
@@ -387,10 +405,12 @@ class SchemeGrammar extends GrammarDefinition {
     pattern("^|\\"),
   ].toChoiceParser();
 
-  Parser<bool> boolean() => [
-    [string("#true"), string("#t")].toChoiceParser().mapTo(true),
-    [string("#false"), string("#f")].toChoiceParser().mapTo(false),
-  ].toChoiceParser() as Parser<bool>;
+  Parser<bool> boolean() =>
+      [
+            [string("#true"), string("#t")].toChoiceParser().mapTo(true),
+            [string("#false"), string("#f")].toChoiceParser().mapTo(false),
+          ].toChoiceParser()
+          as Parser<bool>;
 
   Parser character() => [
     seq2(string("#\\"), characterName()),
@@ -409,10 +429,16 @@ class SchemeGrammar extends GrammarDefinition {
     string("tab"),
   ].toChoiceParser();
 
-  Parser sString() => seq3(doubleQuote(), stringElement().star(), doubleQuote());
+  Parser sString() =>
+      seq3(doubleQuote(), stringElement().star(), doubleQuote());
   Parser stringElement() => [
     inlineHexEscape(),
-    seq4(char(r"\"), intralineWhitespace().star(), lineEnding(), intralineWhitespace().star()),
+    seq4(
+      char(r"\"),
+      intralineWhitespace().star(),
+      lineEnding(),
+      intralineWhitespace().star(),
+    ),
     mnemonicEscape(),
     string(r'\"'),
     string(r"\\"),
@@ -421,9 +447,92 @@ class SchemeGrammar extends GrammarDefinition {
   ].toChoiceParser();
 
   Parser byteVector() => seq3(hashU8Paren(), sByte().star(), rParen());
-  Parser sByte() => fail("TODO") // integer value from 0-255
+  Parser sByte() => failure(message: "TODO"); // integer value from 0-255
 
+  Parser number() => [sNum(2), sNum(8), sNum(10), sNum(16)].toChoiceParser();
 
+  Parser sNum(int r) => seq2(prefix(r), complex(r));
+  Parser complex(int r) => [
+    real(r),
+    seq3(real(r), char("@"), real(r)),
+    seq4(real(r), char("+"), ureal(r), char("i", ignoreCase: true)),
+    seq4(real(r), char("-"), ureal(r), char("i", ignoreCase: true)),
+    seq3(real(r), char("+"), char("i", ignoreCase: true)),
+    seq3(real(r), char("-"), char("i", ignoreCase: true)),
+    seq3(real(r), infnan(), char("i", ignoreCase: true)),
+    seq3(char("+"), ureal(r), char("i", ignoreCase: true)),
+    seq3(char("-"), ureal(r), char("i", ignoreCase: true)),
+    seq2(infnan(), char("i", ignoreCase: true)),
+    string("+i", ignoreCase: true),
+    string("-i", ignoreCase: true),
+  ].toChoiceParser();
+  Parser real(int r) => [seq2(sign(), ureal(r)), infnan()].toChoiceParser();
+  Parser ureal(int r) => [
+    uinteger(r),
+    seq3(uinteger(r), char("/"), uinteger(r)),
+    decimal(r),
+  ].toChoiceParser();
+  Parser decimal(int r) {
+    if (r != 10) {
+      return failure(message: "decimal only defined for radix 10");
+    } else {
+      return [
+        seq2(uinteger(10), suffix()),
+        seq3(dot(), digit10().plus(), suffix()),
+        seq4(digit10().plus(), dot(), digit10().star(), suffix()),
+      ].toChoiceParser();
+    }
+  }
+
+  Parser uinteger(int r) => digit(r).plus();
+  Parser prefix(int r) => [
+    seq2(radix(r), exactness()),
+    seq2(exactness(), radix(r)),
+  ].toChoiceParser();
+  Parser infnan() => [
+    string("+inf.0", ignoreCase: true),
+    string("-inf.0", ignoreCase: true),
+    string("+nan.0", ignoreCase: true),
+    string("-nan.0", ignoreCase: true),
+  ].toChoiceParser();
+
+  Parser suffix() =>
+      seq3(exponentMarker(), sign(), digit10().plus()).optional();
+  Parser exponentMarker() => char("e", ignoreCase: true);
+  Parser sign() => pattern("+-").optional();
+  Parser exactness() =>
+      seq2(char("#"), pattern("ie", ignoreCase: true)).optional();
+  Parser radix(int r) {
+    if (r == 2) {
+      return string("#b", ignoreCase: true);
+    } else if (r == 8) {
+      return string("#o", ignoreCase: true);
+    } else if (r == 16) {
+      return string("#x", ignoreCase: true);
+    } else if (r == 10) {
+      return string("#d", ignoreCase: true).optional();
+    } else {
+      throw ArgumentError(
+        "only a radix of 2, 8, 10, or 16 is allowed, given $r",
+      );
+    }
+  }
+
+  Parser digit(int r) {
+    if (r == 2) {
+      return pattern("01");
+    } else if (r == 8) {
+      return pattern("0-7");
+    } else if (r == 10) {
+      return digit10();
+    } else if (r == 16) {
+      return [digit10(), pattern("abcdef", ignoreCase: true)].toChoiceParser();
+    } else {
+      throw ArgumentError(
+        "only a radix of 2, 8, 10, or 16 is allowed, given $r",
+      );
+    }
+  }
 
   Parser syntacticKeyword() => [
     expressionKeyword(),
@@ -459,34 +568,6 @@ class SchemeGrammar extends GrammarDefinition {
 
   Parser variable() => ref0(keyword).not().seq(identifier());
 
-  Parser<SBoolean> boolean() =>
-      [
-        string("#t").token().map((t) => SBoolean(true, t)),
-        string("#f").token().map((t) => SBoolean(false, t)),
-      ].toChoiceParser(
-        failureJoiner: (f1, f2) {
-          assert(
-            f1.position == f2.position,
-            "the positions should be the same",
-          );
-          return Failure(f1.buffer, f1.position, err.boolean);
-        },
-      );
-
-
-
-  Parser<SString> sString() => seq3(
-    char('"'),
-    ref0(stringElement).star(),
-    char('"'),
-  ).map3((_, chs, _) => chs.join("")).token().map((t) => SString(t.value, t));
-
-  Parser<String> stringElement() => [
-    string('\\"').map((s) => '"'),
-    string('\\\\').map((s) => "\\"),
-    pattern('^\\"', unicode: true),
-  ].toChoiceParser();
-
   Parser sElse() => string("else");
   Parser arrow() => string("=>");
   Parser<String> define() => string("define");
@@ -513,149 +594,6 @@ class SchemeGrammar extends GrammarDefinition {
   Parser quasiquote() => string("quasiquote");
   Parser letSyntax() => string("let-syntax");
   Parser letrecSyntax() => string("letrec-syntax");
-
-  // Numbers
-
-  Parser<SNumber> number() => [
-    ref1(sNum, 2),
-    ref1(sNum, 8),
-    ref1(sNum, 10),
-    ref1(sNum, 16),
-  ].toChoiceParser();
-
-  Parser<SNumeric> sNum(int r) => seq2(ref1(prefix, r), ref1(complex, r));
-
-  Parser plusOrMinus() => pattern("+-");
-
-  Parser complex(int r) => [
-    seq3(ref1(real, r), char("@"), ref1(real, r)),
-    seq4(
-      ref1(real, r),
-      plusOrMinus(),
-      ref(ureal, r),
-      char("i", ignoreCase: true),
-    ),
-    seq3(ref1(real, r), plusOrMinus(), char("i", ignoreCase: true)),
-    seq2(plusOrMinus(), ref1(ureal, r)),
-    seq2(plusOrMinus(), char("i", ignoreCase: true)),
-    ref1(real, r),
-  ].toChoiceParser();
-
-  Parser real(int r) => seq2(ref0(sign).optional(), ref1(ureal, r));
-
-  Parser ureal(int r) => [
-    ref(pointed, r),
-    seq3(ref1(uinteger, r), char("/"), ref1(uinteger, r)),
-    ref1(uinteger, r),
-  ].toChoiceParser();
-
-  Parser pointed(int r) => [
-    seq2(ref1(uinteger, r), ref1(suffix, r)),
-    seq4(
-      char("."),
-      ref1(digit, r).plus(),
-      char("#").star(),
-      ref1(suffix, r).optional(),
-    ),
-    seq5(
-      ref1(digit, r).plus(),
-      char("."),
-      ref1(digit, r).star(),
-      char("#").star(),
-      ref1(suffix, r).optional(),
-    ),
-    seq5(
-      ref1(digit, r).plus(),
-      char("#").plus(),
-      char("."),
-      char("#"),
-      ref1(suffix, r).optional(),
-    ),
-  ].toChoiceParser();
-
-  Parser<SNumber> uinteger(int r) => seq2(ref1(digit, r), char("#").star())
-      .map2(
-        (digits, hashes) => // int and whether it's exact
-        (
-          BigInt.parse(digits + "0" * hashes.length, radix: r),
-          hashes.isEmpty,
-        ),
-      )
-      .token()
-      .map((Token<(BigInt, bool)> t) {
-        var (bi, isExact) = t.value;
-        Token<String> stringToken = t.toStringToken;
-        return isExact
-            ? SNumber(SInt(bi), stringToken)
-            : SNumber(SDouble(bi.toDouble()), stringToken);
-      });
-
-  Parser<(int, Exactness)> prefix(int r) => [
-    seq2(
-      ref1(radix, r),
-      ref0(exactness).optionalWith(Exactness.exact),
-    ).map2((rad, isExact) => (rad, isExact)),
-    seq2(
-      ref0(exactness).optionalWith(Exactness.exact),
-      ref1(radix, r),
-    ).map2((isExact, rad) => (rad, isExact)),
-  ].toChoiceParser();
-
-  Parser suffix(int r) => seq3(
-    ref1(exponentMarker, r),
-    ref0(sign).optional(),
-    ref1(digit, r).plus(),
-  );
-
-  Parser exponentMarker(int r) {
-    if (r == 16) {
-      return anyOf("ls", ignoreCase: true);
-    } else {
-      return anyOf("defls", ignoreCase: true);
-    }
-  }
-
-  Parser sign() => anyOf("+-");
-
-  Parser<Exactness> exactness() => seq2(
-    char("#"),
-    char("e", ignoreCase: true).mapTo(Exactness.exact) |
-        char("i", ignoreCase: true).mapTo(Exactness.inexact),
-  ).map2((_, ex) => ex);
-
-  // TODO: clean up the cases
-  Parser<int> radix(int r) {
-    if (r == 2) {
-      return seq2(char("#"), char("b", ignoreCase: true)).map((_) => 2);
-    } else if (r == 8) {
-      return seq2(char("#"), char("o", ignoreCase: true)).map((_) => 8);
-    } else if (r == 10) {
-      return [
-        seq2(char("#"), char("d", ignoreCase: true)),
-        epsilon(),
-      ].toChoiceParser().map((_) => 10);
-    } else if (r == 16) {
-      return seq2(char("#"), char("x", ignoreCase: true)).map((_) => 16);
-    } else {
-      throw ArgumentError(
-        "only a radix of 2, 8, 10, or 16 is allowed, given $r",
-      );
-    }
-  }
-
-  Parser digit(int r) {
-    if (r == 2) {
-      return anyOf("01");
-    } else if (r == 8) {
-      return anyOf("01234567");
-    } else if (r == 10) {
-      return anyOf("0123456789");
-    } else if (r == 16) {
-      return anyOf("012345789abcdef", ignoreCase: true);
-    } else {
-      throw ArgumentError("only a radix of 2, 8, 10, or 16 is allowed");
-    }
-  }
 }
 
 extension ToStringToken<T> on Token<T> {
